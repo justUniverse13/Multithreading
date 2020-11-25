252b5a62acea554f2deb0079f6bd7174000edfcb
Bohdan Tsiunyk <44728281+justUniverse13@users.noreply.github.com>
2020-11-25 12:27:26 +0200

    initializing

---
+++ Multithreading/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Multithreading
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int queueLength = 20;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MyQueue<int> secondThreadQueue = new MyQueue<int>();
+
+        private void StartFirstThread_Click(object sender, RoutedEventArgs e)
+        {
+            Thread firstThread = new Thread(GenerateNumbersByFirstThread);
+            
+            if (StartFirstThread.Content.ToString() == "StartFirstThread")
+            {
+                StartFirstThread.Content = "StopFirstThread";
+                firstThread.Start();
+            }
+            else
+            {
+                StartFirstThread.Content = "StartFirstThread";
+                firstThread.Abort();
+            }
+        }
+
+        private void GenerateNumbersByFirstThread()
+        {
+            for (int i = 0; ; i++)
+            {
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    FirstThreadListBox.Items.Add(i);
+                    if (i >= queueLength)
+                    {
+                        secondThreadQueue.Dequeue();
+                        SecondThreadListBox.Items.Add(i);
+                        SecondThreadListBox.Items.Remove(i-20);
+                        secondThreadQueue.Enqueue(i);
+                    }
+                    else
+                    {
+                       secondThreadQueue.Enqueue(i);
+                       SecondThreadListBox.Items.Add(secondThreadQueue.Last);
+                    }
+                }));
+                Thread.Sleep(1000);
+            }
+        }  
+
+        private void GetOldestValueFromQueue()
+        {
+            for(; ; )
+            {
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    ThirdThreadListBox.Items.Add(secondThreadQueue.Last);
+                }));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void StartThirdThread_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thirdThread = new Thread(GetOldestValueFromQueue);
+               
+              
+            if (StartThirdThread.Content.ToString() == "StartThirdThread")
+            {
+            StartThirdThread.Content = "StopThirdThread";
+                thirdThread.Start();
+            }
+            else
+            {
+
+             StartThirdThread.Content = "StartThirdThread";
+                thirdThread.Abort();
+            }
+        }
+    }
+}
---
+++ Multithreading/MyQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    public class MyQueue<T>
+    {
+        private List<T> _items;
+        private int index;
+
+        public int Count
+        {
+            get
+            {
+                return _items.Count;
+            }
+        }
+
+        public T Last
+        {
+            get
+            {
+                return _items[index];
+            }
+        }
+
+        public T First
+        {
+            get
+            {
+                return _items[0];
+            }
+        }
+
+        public MyQueue()
+        {
+            index = -1;
+            _items = new List<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            _items.Add(item);
+            index++;
+        }
+
+        public T Dequeue()
+        {
+            T i = _items[0];
+            _items.RemoveAt(0);
+            index--;
+            return i;
+        }
+
+        public T Peek()
+        {
+            return _items[0];
+        }
+    }
+}
